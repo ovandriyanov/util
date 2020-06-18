df3b8cacde4b7082fd09ba645841d93ccc3b46bb
Oleg Andriyanov <o.andriyanov@gmail.com>
2020-06-18 13:24:43 +0300

    Expand variables

--- console/c.sh
+++ console/c.sh
@@ -7,5 +7,6 @@ function c() {
     fi
 
     EXPANDED_PATH="`${HOME}/bin/expand-path ${1}`"
+    EXPANDED_PATH="`bash -c \"echo ${EXPANDED_PATH}\"`"
     [ -n "${EXPANDED_PATH}" ] && cd ${EXPANDED_PATH}
 }
